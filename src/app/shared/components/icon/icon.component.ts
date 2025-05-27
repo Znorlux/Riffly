cea1d57a12206255fba8d9bea9dc07184f575b62
Znorlux <christianlombardi0414@gmail.com>
2025-05-27 01:30:57 -0500

    Add collapsible sidebar functionality with animations and new icons

--- src/app/shared/components/icon/icon.component.ts
+++ src/app/shared/components/icon/icon.component.ts
@@ -21,7 +21,9 @@ export type IconName =
   | 'clock'
   | 'views'
   | 'logo'
-  | 'logout';
+  | 'logout'
+  | 'chevron-left'
+  | 'chevron-right';
 
 @Component({
   selector: 'app-icon',
@@ -190,6 +192,24 @@ export type IconName =
           stroke-linejoin="round"
           stroke-width="2"
           d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+        ></path>
+
+        <!-- Chevron Left -->
+        <path
+          *ngSwitchCase="'chevron-left'"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 19l-7-7 7-7"
+        ></path>
+
+        <!-- Chevron Right -->
+        <path
+          *ngSwitchCase="'chevron-right'"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 5l7 7-7 7"
         ></path>
       </ng-container>
     </svg>
