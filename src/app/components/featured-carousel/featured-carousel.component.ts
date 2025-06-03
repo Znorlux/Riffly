cce1db56a2561fc98b61d6e4711b6f0474402534
Znorlux <christianlombardi0414@gmail.com>
2025-06-02 20:52:12 -0500

    Add navigation to create page from featured carousel and remove unused music generation input

--- src/app/components/featured-carousel/featured-carousel.component.ts
+++ src/app/components/featured-carousel/featured-carousel.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-featured-carousel',
   templateUrl: './featured-carousel.component.html',
 })
 export class FeaturedCarouselComponent {
+  constructor(private router: Router) {}
   // Implementation details
+  onStartCreating() {
+    this.router.navigate(['/create']);
+  }
 }
